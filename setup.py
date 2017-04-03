d083b1a92ba16561468e5975435a5128116b17af
Luka Čehovin <luka.cehovin@gmail.com>
2017-04-03 15:17:32 +0200

    Optional paramiko install.

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+from pkg_resources import WorkingSet , DistributionNotFound
+working_set = WorkingSet()
+
+requirements = ["watchdog>=0.8.0"]
+
+try:
+    working_set.require('paramiko>=1.16.0')
+except DistributionNotFound:
+    reqirements.append('paramiko>=1.16.0')
 
 setup(name='FolderSync',
 	version='0.1.0',
@@ -10,5 +19,5 @@ setup(name='FolderSync',
 	url='https://github.com/lukacu/python-foldersync/',
 	packages=['foldersync', 'foldersync.storage', 'foldersync.processors'],
 	scripts=["bin/folderwatch", "bin/folderexport"],
-    install_requires=["watchdog>=0.8.0", "paramiko>=1.16.0"],
+    install_requires=requirements,
 )
